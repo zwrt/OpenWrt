836b1d9fd1f7d59304ba1fa283e378939075df1e
zwrt <155215126+zwrt@users.noreply.github.com>
2024-02-07 17:48:43 +0800

    Update diy-part2.sh

--- R2S/diy-part2.sh
+++ R2S/diy-part2.sh
@@ -28,26 +28,3 @@ sed -i "s/<%=pcdata(ver.distname)%> <%=pcdata(ver.distversion)%>/<%=pcdata(ver.d
 
 # 修改概览里时间显示为中文数字
 sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/lean/autocore/files/arm/index.htm
-
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
-
-# 添加额外插件
-git clone --depth 1 https://github.com/kenzok78/luci-app-adguardhome package/luci-app-adguardhome
-git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
-git clone --depth 1 https://github.com/Lienol/openwrt-package package/luci-app-filebrowser
-
-# 科学上网插件
-git clone --depth 1 https://github.com/ipenwrt/luci-app-vssr package/luci-app-vssr
-git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
-git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
