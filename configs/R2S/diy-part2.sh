0b59934df6621b0c182b126d6a77e7a90058d3c7
zwrt <155215126+zwrt@users.noreply.github.com>
2024-02-11 16:11:05 +0800

    Update diy-part2.sh

--- configs/R2S/diy-part2.sh
+++ configs/R2S/diy-part2.sh
@@ -38,3 +38,16 @@ sed -i "s/<%=pcdata(ver.distname)%> <%=pcdata(ver.distversion)%>/<%=pcdata(ver.d
 
 # 修改概览里时间显示为中文数字
 sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/lean/autocore/files/arm/index.htm
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 添加额外插件
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
